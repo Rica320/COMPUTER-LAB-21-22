42622749103a6e4246a81fadd73ef5e056c59042
PedroCAB <up202006086@up.pt>
2022-03-24 12:28:45 +0000

    Added some notes and finished utils.c

--- lab2/lab2/timer.c
+++ lab2/lab2/timer.c
@@ -5,13 +5,22 @@
 
 #include "i8254.h"
 
-int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
+/*Changes the operating sequence of a timer
+Must use the read back command so it doesn't change the 4 LSB's (mode and BCD binary) of the timer's control word
+timer = Timer to configure (ranges from 0 to 2)
+freq =  Timer operating frequency
+return: 0 on sucess and non-zero otherwise
+*/
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   return 1;
 }
 
+/*Subscribes and enables Timer 0 interrupts
+bit_no	= address of memory to be initialized with the bit number to be set in the mask returned upon an interrupt
+return: 0 upon success and non-zero otherwise
+*/
 int (timer_subscribe_int)(uint8_t *bit_no) {
     /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
@@ -19,6 +28,9 @@ int (timer_subscribe_int)(uint8_t *bit_no) {
   return 1;
 }
 
+/*Unsubscribes Timer 0 interrupts.
+Return: 0 upon success and non-zero otherwise
+*/
 int (timer_unsubscribe_int)() {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
@@ -26,11 +38,19 @@ int (timer_unsubscribe_int)() {
   return 1;
 }
 
+/*Timer 0 interrupt handler.
+Increments counter
+*/
 void (timer_int_handler)() {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 }
 
+/*Reads the input timer configuration (status) via read-back command.
+timer = Timer whose configuration to read (Ranges from 0 to 2)
+st = Address of memory position to be filled with the timer config
+Return 0 upon success and non-zero otherwise
+*/
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
@@ -38,6 +58,12 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   return 1;
 }
 
+/*Shows timer configuration.
+Displays, in a human friendly way, the specified field of a timer status, which was read via the read-back command
+timer = timer whose configuration should be displayed (Ranges from 0 to 2)
+st = status read via the read-back command
+field = status field to display in human friendly way
+*/
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
   /* To be implemented by the students */
--- lab2/lab2/utils.c
+++ lab2/lab2/utils.c
@@ -2,23 +2,31 @@
 
 #include <stdint.h>
 
+//Returns the LSB (least significant bit) of a 2 byte integer
+//val = 2 byte integer
+//lsb = address of memory location to be updated with val's LSB
+//Return 0 upon success and non-zero otherwise
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *lsb = (uint8_t)(val & 1);
+  return 0;
 }
 
+//Returns the MSB (most significant bit) of a 2 byte integer
+//val = 2 byte integer
+//msb = address of memory location to be updated with val's LSB
+//Return 0 upon success and non-zero otherwise
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *msb = (uint8_t)(val & 32767);
+  return 0;
 }
 
-int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+//Invokes sys_inb() system call but reads the value into a uint8_t variable.
+//port = the input port that is to be read
+//value =address of 8-bit variable to be update with the value read
+//Return 0 upon success and non-zero otherwise
+int(util_sys_inb)(int port, uint8_t *value) {
+  uint32_t a;
+  sys_inb(port, uint32_t & a);
+  *value = (uint8_t) a;
+  return 0;
 }

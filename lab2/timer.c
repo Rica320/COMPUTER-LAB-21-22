60737467841f48a33c34379da63b0f4763698514
Guilherme Freire <up202004809@fe.up.pt>
2022-04-01 15:30:20 +0100

    timer_set_frequency

--- lab2/timer.c
+++ lab2/timer.c
@@ -4,13 +4,56 @@
 #include <stdint.h>
 
 #include "i8254.h"
+unsigned int count = 0;
+int hook_id = 2;
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+   uint8_t st;
 
-  return 1;
+  if (timer_get_conf(timer, &st) != 0) {
+    printf("Error getting configuration in timer_set_frequency\n");
+    return 1;
+  }
+
+  uint8_t ctrlw;
+  uint8_t lsb, msb;
+
+  uint16_t hrtz = TIMER_FREQ / freq;
+
+  util_get_LSB(hrtz , &lsb);
+  util_get_MSB(hrtz , &msb);
+
+  switch (timer) {
+    case 0:
+      ctrlw = ( TIMER_SEL0 | TIMER_LSB_MSB | (st & 0x0F));
+      sys_outb(TIMER_CTRL, ctrlw);
+      sys_outb(TIMER_0, lsb);
+      sys_outb(TIMER_0, msb);
+      break;
+    
+    case 1:
+      ctrlw = ( TIMER_SEL1 | TIMER_LSB_MSB | (st & 0x0F));
+      sys_outb(TIMER_CTRL, ctrlw);
+      sys_outb(TIMER_1, lsb);
+      sys_outb(TIMER_1, msb);
+      break;
+
+    case 2:
+      ctrlw = ( TIMER_SEL2 | TIMER_LSB_MSB | (st & 0x0F));
+      sys_outb(TIMER_CTRL, ctrlw);
+      sys_outb(TIMER_2, lsb);
+      sys_outb(TIMER_2, msb);
+      break;
+
+    default:
+      printf("Error selecting Timer\n");
+      return 1;
+  }
+
+  return 0;
 }
+
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
     /* To be implemented by the students */

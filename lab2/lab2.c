6fb50f41270d977e648573dfb567915cfedae978
Guilherme Freire <up202004809@fe.up.pt>
2022-04-01 15:21:41 +0100

    lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -31,21 +31,63 @@ int main(int argc, char *argv[]) {
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t st;
 
+  if(timer_get_conf(timer, &st)==0)
+    if(timer_display_conf(timer, st, field)==0) return 0;
+  printf("Error in function timer_test_read_config.\n");
   return 1;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+ if (timer_set_frequency(timer, freq) == 1) {
+    printf("Error setting frequency.\n");
+    return 1;
+  }
+  return 0;
 }
 
 int(timer_test_int)(uint8_t time) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  uint8_t bit_no;
+  int ipc_status, r;
+  message msg;
+  count = 0;
 
-  return 1;
+  if (timer_subscribe_int(&bit_no)!=0) {
+      printf("Error subscribing timer interrupts.\n");
+      return 1;
+  }
+
+  while (count/sys_hz() < time) {
+    /* Get a request message. */
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & BIT(bit_no)) { /* subscribed interrupt*/
+            timer_int_handler();
+            if( (count%sys_hz()) == 0 ) {
+              timer_print_elapsed_time();
+            }
+          }
+          break;
+        
+        default:
+          break;
+      }
+    }
+  }
+
+  if (timer_unsubscribe_int()!=0) {
+    printf("Error unsubscribing timer interrupts\n");
+    return 1;
+  }
+
+  return 0;
 }

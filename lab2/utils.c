389fe7b414e6d2b4b4de56910f8d83bb4eea7e4a
Guilherme Freire <up202004809@fe.up.pt>
2022-04-01 15:15:29 +0100

    Untils

--- lab2/utils.c
+++ lab2/utils.c
@@ -4,21 +4,27 @@
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(lsb==NULL) return 1;
+  *lsb = (uint8_t) val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(msb==NULL) return 1;
+  *msb = (uint8_t) (val >> 8);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t new_val;
+  
+  if(sys_inb(port, &new_val)!=0){
+    printf("Error in util_sys_inb\n");
+    return 1;
+  }
+  *value=new_val & 0xFF;
+  
+  return 0;
 }

9b07c0cc25850cbc89aabe851f49abc7978fec50
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-05-15 02:33:23 +0100

    img moving acording sync with timer

--- proj/src/drivers/keyboard/kbd_keys.h
+++ proj/src/drivers/keyboard/kbd_keys.h
@@ -6,4 +6,9 @@
 #define UP_ARROW 0x48
 #define DOWN_ARROW 0x50
 
+#define RELEASE_RIGHT_ARROW 0xcd
+#define RELEASE_LEFT_ARROW 0xcb
+#define RELEASE_UP_ARROW 0xc8
+#define RELEASE_DOWN_ARROW 0xd0
+
 #endif

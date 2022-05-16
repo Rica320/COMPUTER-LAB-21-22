7c901ba7c448fdd0933e3e66caa99ad1f41ede93
PedroCAB <up202006086@up.pt>
2022-05-16 21:45:18 +0100

    Piece Implementation Stubs and Useful Macros

--- proj/src/game/objects/pieces/pieces.c
+++ proj/src/game/objects/pieces/pieces.c
@@ -1,1 +1,25 @@
 #include "pieces.h"
+
+uint64_t get_Pawn_valid_moves(uint8_t lin, uint8_t col) {
+	//Falta implementar movimentações para comer peças
+  uint64_t answer = 0;
+	if (lin == 6) { //se estão na posição inicial
+    answer = answer || POS(5, col) || POS(4, col);
+  }
+	else {
+    answer = answer || POS(5, col);
+  }
+  return answer;
+}
+
+uint64_t get_Bishop_valid_moves(uint8_t lin, uint8_t col) {
+	//Not implemented yet!
+}
+  //Auxiliar
+uint8_t get4LSB(uint8_t val) {
+  return val && 0x0F;
+}
+
+uint8_t get4MSB(uint8_t val) {
+  return val>>4;
+}

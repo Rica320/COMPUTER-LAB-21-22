f697f071195470f0c4cef1d012c38ba972125d5b
Guilherme Freire <up202004809@fe.up.pt>
2022-05-18 16:09:31 +0100

    Function that tells if square is white or black

--- proj/src/game/objects/board/board.c
+++ proj/src/game/objects/board/board.c
@@ -1,2 +1,7 @@
 #include "board.h"
 
+bool get_square_color(uint8_t lin, uint8_t col){//returns true if white and false otherwise
+  
+  if((lin+col)%2) != 0 return true;
+  return false;  
+}
--- proj/src/game/objects/board/board.h
+++ proj/src/game/objects/board/board.h
@@ -6,5 +6,5 @@
 #define BOARD_SIZE 8
 extern board* table;
 typedef struct Piece board[BOARD_SIZE][BOARD_SIZE];
-
+bool get_square_color(uint8_t lin, uint8_t col); //returns true if white and false otherwise.
 #endif

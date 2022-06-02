6e31a23f81c7128bc1552f34d830ea9c6d77dc62
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-06-02 11:46:21 +0100

    Started Mouse Documentation

--- proj/src/drivers/mouse/mouse.h
+++ proj/src/drivers/mouse/mouse.h
@@ -7,13 +7,30 @@
 #include "../../assets/cursor.h"
 
 typedef struct packet packet_t;
-
+/**
+ * @brief Parses mouse data into a packet struct data type
+ * @param data Data obtained from the mouse
+ * @return Data parsed to a Packet Struct 
+ */
 packet_t mouse_data_to_packet(uint8_t* data);
+/**
+ * @brief Disables Mouse Data Reporting using kbc_send_mouse_cmd()
+ * @return EXIT_SUCCESS on success
+ */
 int mouse_disable_data_reporting();
+/**
+ * @brief Enables Mouse Data Reporting using kbc_send_mouse_cmd()
+ * @return EXIT_SUCCESS on success
+ */
 int _mouse_enable_data_reporting_();
 
 typedef sprite_t * mouse_ptr;
-
+/**
+ * @brief 
+ * 
+ * @param pp 
+ * @return struct mouse_ev* 
+ */
 struct mouse_ev* mouse_get_event(struct packet *pp);
 
 #endif

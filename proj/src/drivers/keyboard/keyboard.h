6c485b599f1cecdb401e1f8d32fc6ec630cc0335
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-06-02 11:41:04 +0100

    Kbd documentation done

--- proj/src/drivers/keyboard/keyboard.h
+++ proj/src/drivers/keyboard/keyboard.h
@@ -5,12 +5,33 @@
 
 #include "../kbc/kbc.h"
 
+/**
+ * @brief Reads Keyboard Scancode Bytes from the KBC Output Buffer
+ * @param code[] Array where the Scancode Bytes are stored after reading
+ * @param *size Size of the code[] array
+ * @return EXIT_SUCCESS on success
+ */
 int (kbd_poll)(uint8_t code[], uint8_t *size);
+/**
+ * @brief Sets bits 0 and 1 of the Output Buffer to 1
+ * @return EXIT_SUCCESS on success
+ */
 int (kbd_restore)(); 
+/**
+ * @brief Tests the KBC Interface
+ * @return Status Byte Read from the Status Register
+ */
 int (kbc_kbd_interface_cmd)();
 
-
+/**
+ * @brief Enables KBC Interface
+ * @return EXIT_SUCCESS on success
+ */
 int (kbc_enable_kbd_cmd)(); // I THINK IT IS ALSO WORKING
+/**
+ * @brief Disables KBC Interface
+ * @return EXIT_SUCCESS on success
+ */
 int (kbc_disable_kbd_cmd)(); // WORKING
 
 

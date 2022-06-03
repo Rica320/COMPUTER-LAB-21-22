c580744518ae5c2f1ef4c9b2e9319d6f200e49b0
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-06-03 15:14:17 +0100

    Pieces Documentation

--- proj/src/game/objects/pieces/pieces.h
+++ proj/src/game/objects/pieces/pieces.h
@@ -50,22 +50,107 @@ typedef struct Piece Piece_t;
 Piece_t *make_piece(const xpm_map_t xpm, PIECE_T p_t, Piece_Color color);
 void free_piece(Piece_t *p);
 
-
+//**
+ * @brief Get the valid moves object
+ * 
+ * @param board 
+ * @param lin 
+ * @param col 
+ * @param valid_moves 
+ * @param isWhitesTurn 
+ * @return uint64_t 
+ */
 uint64_t get_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8], bool isWhitesTurn); // IGNORE THIS FOR NOW
 
 // do this types of functs for every piece ...
+/**
+ * @brief Get the valid moves for a Pawn
+ * 
+ * @param board Current board struct
+ * @param lin Line where the Pawn is
+ * @param col Column where the Pawn is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_Pawn_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
+/**
+ * @brief Get the valid moves for a Bishop
+ * 
+ * @param board Current board struct
+ * @param lin Line where the Bishop is
+ * @param col Column where the Bishop is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_Bishop_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
+/**
+ * @brief Get the valid moves for a Queen
+ * 
+ * @param board Current board struct
+ * @param lin Line where the Queen is
+ * @param col Column where the Queen is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_Queen_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
+/**
+ * @brief Get the valid moves for a King
+ * 
+ * @param board Current board struct
+ * @param lin Line where the King is
+ * @param col Column where the King is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_King_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
+/**
+ * @brief Get the valid moves for a Rook
+ * 
+ * @param board Current board struct
+ * @param lin Line where the Rook is
+ * @param col Column where the Rook is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_Rook_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
+/**
+ * @brief Get the valid moves for a Knight
+ * 
+ * @param board Current board struct
+ * @param lin Line where the Knight is
+ * @param col Column where the Knight is
+ * @param valid_moves Struct with bools that are true when move is valid
+ * @return uint64_t with valid positions set to 1
+ */
 uint64_t get_Knight_valid_moves(Board board[8][8], uint8_t lin, uint8_t col, bool valid_moves[8][8]);
 
 // Auxiliar
-
+/**
+ * @brief Checks if there is an Enemy Piece in position pos
+ * 
+ * @param pos Struct with the piece position
+ * @param color Represents the piece color
+ * @return true if there is an Enemy Piece in pos
+ * @return false otherwise
+ */
 bool isEnemyPiecePos(Piece_t *pos, int color);
+/**
+ * @brief Checks if there is another Ally Piece in position pos
+ * 
+ * @param pos Struct with the piece position
+ * @param color Represents the piece color
+ * @return true if there is an Ally Piece in pos
+ * @return false otherwise
+ */
 bool isOwnPiecePos(Piece_t *pos, int color);
-
+/**
+ * @brief Checks if a certain position is inside the board
+ * 
+ * @param lin Line of the position
+ * @param col Column of the position
+ * @return true if (lin,col) is inside the board
+ * @return false otherwise
+ */
 bool is_inside_board(uint8_t lin, uint8_t col);
 
 #endif

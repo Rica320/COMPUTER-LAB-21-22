68667fb8ed56dbacb391c71c8861bd1f6adcaf4e
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-06-11 12:56:28 +0100

    rtc documentation

--- proj/src/drivers/rtc/rtc.h
+++ proj/src/drivers/rtc/rtc.h
@@ -33,16 +33,40 @@
 #define RTC_AF BIT(5) // Alarm Interrupt Pending
 #define RTC_PF BIT(6) // Periodic Interrupt Pending
 
+/**
+ * @brief Enables/Disables Interrupt Updates for the RTC
+ * @param enable True for enabling, False for disabling
+ * @return 0 on success
+ */
 int(enable_int_update)(bool enable);
-
+/**
+ * @brief Subscribes RTC Interrupts
+ * @param bit_no Identifier of the interrupt to be subscribed
+ * @return 0 on success
+ */
 int(rtc_subscribe_int)(uint8_t *bit_no);
-
+/**
+ * @brief Unsubscribes RTC Interrupts
+ * @return 0 on success
+ */
 int(rtc_unsubscribe_int)();
-
+/**
+ * @brief Handles RTC Interrupts
+ */
 void(rtc_ih)();
-
+/**
+ * @brief Waits for the RTC to update its values
+ * @return 0 on success
+ */
 int(wait_rtc)();
-
+/**
+ * @brief Converts Binary Coded Decimal to Decimal
+ * @param bcdNum Value expressed in Binary Coded Decimal
+ * @return bcdNum value expressed in Decimal
+ */
 uint8_t(bcd2dec)(uint8_t bcdNum);
-
+/**
+ * @brief Reads Value from Argument Field and Stores it in the global variable
+ * @return 0 on success
+ */
 int(rtc_read)(uint8_t reg, uint8_t pos);
